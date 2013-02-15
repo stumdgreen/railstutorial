a3c03cc5d573a585b751a192fd5e72cddf54dffb
M. David <mdavidgreen@gmail.com>
2013-02-15 12:15:51 -0800

    User profile page test and styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Returns the Gravater (http://gravatar.com) for a given user
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "//secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", width: 80, height: 80)
+  end
 end
