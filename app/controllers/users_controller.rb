a0c862957efc1eaef4798f72d7fbb77a2eb95692
M. David <mdavidgreen@gmail.com>
2013-02-17 22:42:08 -0800

    User edit page working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updates"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
