8b31f54f098baf8cc149b92c96ebb6cf8ba3e78c
M. David <mdavidgreen@gmail.com>
2013-02-21 00:24:18 -0800

    Admin users alone can delete other users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,24 @@ describe 'User Page' do
       end
     end
 
+    describe "delete links" do
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it { should_not have_link('delete', href: user_path(admin)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+      end
+    end
+
     before do
       sign_in FactoryGirl.create(:user)
       FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
