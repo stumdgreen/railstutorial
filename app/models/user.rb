6caee12aa1971d8eba8522679ff436befce922d2
M. David <mdavidgreen@gmail.com>
2013-02-26 12:28:18 -0800

    Micropost validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
