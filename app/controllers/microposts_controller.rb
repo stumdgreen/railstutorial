7c89e1290d14d28b6094db01caff82c630b14c14
M. David <mdavidgreen@gmail.com>
2013-03-01 00:06:48 -0800

    deleting microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -13,6 +14,17 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:notification] = "Micrpost deleted"
+    redirect_to root_url
   end
+
+  private
+
+    def correct_user
+      @micropost = current_user.microposts.find(params[:id])
+    rescue
+      redirect_to root_url
+    end
   
 end
