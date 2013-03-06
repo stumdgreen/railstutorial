272848207b0e3afd3e54b2e8625cb22d513263f0
M. David <mdavidgreen@gmail.com>
2013-03-06 15:06:30 -0800

    fix broken follower tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: "Sign in") }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visitng the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in' ) }
+        end
       end
 
       describe "submitting to the update action" do
